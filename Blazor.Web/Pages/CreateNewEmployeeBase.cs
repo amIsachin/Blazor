9170d5e8bd32a728cb01d2217ff23274a49a5347
amIsachin <amIsachin@hotmail.com>
2023-08-30 21:04:01 +0530

    almost done authentication and authorize

--- Blazor.Web/Pages/CreateNewEmployeeBase.cs
+++ Blazor.Web/Pages/CreateNewEmployeeBase.cs
@@ -1,13 +1,18 @@
 ﻿using Application.Web.Repositories.Interfaces;
 using Domain.Web.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Forms;
+using System.Net;
 
 namespace Blazor.Web.Pages
 {
     public class CreateNewEmployeeBase : ComponentBase
     {
         public EmployeeEntityWeb EmployeeEntityWeb { get; set; } = new();
+
+        [CascadingParameter]
+        public Task<AuthenticationState> AuthenticationStateTask { get; set; }
 
         [Inject]
         public IEmployeeRepository EmployeeRepository { get; set; }
@@ -24,9 +29,23 @@ namespace Blazor.Web.Pages
         protected override async Task OnInitializedAsync()
         {
             this.editContext = new EditContext(EmployeeEntityWeb);
+
+            var authenticationState = await AuthenticationStateTask;
+
+            if (!authenticationState.User.Identity.IsAuthenticated)
+            {
+                string returnUrl = WebUtility.UrlEncode($"/newEmployee");
+
+                NavigationManager.NavigateTo($"/identity/account/login?returnUrl={returnUrl}");
+            }
+
+
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         protected async Task AddNewEmployee()
         {
             var isInserted = await EmployeeRepository.AddNewEmployee(EmployeeEntityWeb);
@@ -35,9 +54,6 @@ namespace Blazor.Web.Pages
             {
                 NavigationManager.NavigateTo("/");
             }
-
-
-
         }
 
     }
